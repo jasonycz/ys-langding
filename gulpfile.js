aacda7ab6beb60aa47b26b069228506bd9d5d8bc
jason yang <jsonyang@163.com>
2016-03-03 17:13:11 +0800

    update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,3 @@
-//在项目的根目录新建gulpfile.js，require需要的module
 var gulp = require('gulp'),
 minifyhtml = require("gulp-minify-html"),
 minifycss = require('gulp-minify-css'),
@@ -6,33 +5,30 @@ concat = require('gulp-concat'),
 uglify = require('gulp-uglify'),
 rename = require('gulp-rename'),
 del = require('del');
-//用来压缩html文件
+//压缩html
 gulp.task('minify_html', function () {
-    gulp.src('*.html') // 要压缩的html文件
-    .pipe(minifyhtml()) //压缩
+    gulp.src('*.html') 
+    .pipe(minifyhtml()) 
     .pipe(gulp.dest('dist/html'));
 });
 //压缩css
 gulp.task('minify_css', function() {
-    return gulp.src('styles/*.css')      //压缩的文件
-        .pipe(minifycss())   //执行压缩
-        .pipe(gulp.dest('dist/css'));   //输出文件夹
+    return gulp.src('styles/*.css')      
+        .pipe(minifycss())  
+        .pipe(gulp.dest('dist/css'));   
         
 });
 //压缩js
 gulp.task('minify_js', function() {
     return gulp.src('scripts/*.js')
-        //.pipe(concat('main.js'))    //合并所有js到main.js
-        //.pipe(gulp.dest('dist/js'))    //输出main.js到文件夹
-        //.pipe(rename({suffix: '.min'}))   //rename压缩后的文件名
-        .pipe(uglify())    //压缩
-        .pipe(gulp.dest('dist/js'));  //输出
+        .pipe(uglify())    
+        .pipe(gulp.dest('dist/js')); 
 });
 //执行压缩前，先删除文件夹里的内容
 gulp.task('clean', function(cb) {
  	del(['dist/css', 'dist/js','dist/html'], cb);
 });
-//默认命令，在cmd中输入gulp后，执行的就是这个命令
+
 gulp.task('default', ['clean','minify_css','minify_js','minify_html']
 );
 
