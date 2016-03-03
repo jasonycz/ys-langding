5a10bcafa0bbb3b2167451eec65e0233fe5e70d2
jason yang <jsonyang@163.com>
2016-03-03 18:16:36 +0800

    update format of all code

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,38 @@
-var gulp = require('gulp');
-var minifyhtml = require("gulp-minify-html");
-var minifycss = require('gulp-minify-css');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
-var del = require('del');
+var gulp = require('gulp')
+var minifyhtml = require("gulp-minify-html")
+var minifycss = require('gulp-minify-css')
+var concat = require('gulp-concat')
+var uglify = require('gulp-uglify')
+var rename = require('gulp-rename')
+var del = require('del')
 
-//压缩html
+// 压缩html
 gulp.task('minify-html', function () {
     gulp.src('*.html') 
     .pipe(minifyhtml()) 
-    .pipe(gulp.dest('dist/html'));
-});
+    .pipe(gulp.dest('dist/html'))
+})
 
-//压缩css
+// 压缩css
 gulp.task('minify-css', function() {
     return gulp.src('styles/*.css')      
         .pipe(minifycss())  
-        .pipe(gulp.dest('dist/css'));         
-});
+        .pipe(gulp.dest('dist/css'))         
+})
 
-//压缩js
+// 压缩js
 gulp.task('minify-js', function() {
     return gulp.src('scripts/*.js')
         .pipe(uglify())    
-        .pipe(gulp.dest('dist/js')); 
-});
+        .pipe(gulp.dest('dist/js'))
+})
 
-//执行压缩前，先删除文件夹里的内容
+// 执行压缩前，先删除文件夹里的内容
 gulp.task('clean', function(cb) {
  	del(['dist/css', 'dist/js','dist/html'], cb);
-});
+})
 
 gulp.task('default', ['clean','minify-css','minify-js','minify-html']
-);
+)
 
 
